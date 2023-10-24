b8c109ad4d65021f4be5625c473b56b8ac4fd176
vasileios20 <vasilis_tsim@outlook.com>
2023-10-24 20:14:21 +0100

    Create base.html and style. Create index.html. Add index view function and urls

--- home/views.py
+++ home/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
-# Create your views here.
+import requests
+import os
+
+TMDB_API_KEY = os.environ.get("TMDB_API_KEY")
+
+
+def index(request):
+    """ A view to return the index page """
+    movies_list = []
+
+    url = f"https://api.themoviedb.org/3/movie/upcoming?api_key={TMDB_API_KEY}&language=en-US&page=1"
+    response = requests.get(url)
+    data = response.json()
+    results = data["results"]
+    temp = []
+    for result in results:
+        temp.append(
+            {"title": result["title"], "overview": result["overview"],
+             "poster_path": result["poster_path"],
+             "release_date": result["release_date"],
+             "movie_id": result["id"]})
+    movies_list.append(temp) if len(temp) > 0 else None
+
+    context = {
+        "movies_list": movies_list,
+    }
+    return render(request, "index.html", context)
