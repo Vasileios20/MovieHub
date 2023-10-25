6c5eeef60d26c969ca1bb62eb30cea27e9de5318
vasileios20 <vasilis_tsim@outlook.com>
2023-10-25 16:42:53 +0100

    Add search function and url for search. Add search_results.html and style

--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 import requests
 import os
 
@@ -27,3 +27,34 @@ def index(request):
         "movies_list": movies_list,
     }
     return render(request, "index.html", context)
+
+
+def search(request):
+    """A view to return the search page"""
+    query = request.GET.get("query")
+    movies_list = []
+
+    if query:
+        url = f"https://api.themoviedb.org/3/search/movie?api_key={TMDB_API_KEY}&query={query}"
+        response = requests.get(url)
+        data = response.json()
+        results = data["results"]
+
+        temp = []
+        for result in results:
+            temp.append(
+                {"title": result["title"],
+                 "poster_path": result["poster_path"],
+                 "release_date": result["release_date"],
+                 "movie_id": result["id"]})
+
+        movies_list.append(temp) if len(temp) > 0 else None
+    else:
+        return HttpResponse("Please enter a search query")
+
+    context = {
+        "query": query,
+        "results_list": movies_list,
+    }
+
+    return render(request, "search_results.html", context)
