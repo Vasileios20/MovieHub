8faf2e3b1656be33739f0fc8f89aa829b4dfe0d0
vasileios20 <vasilis_tsim@outlook.com>
2023-10-29 15:10:31 +0000

    Add script.js to display modal and setTimeout for messages

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,18 @@
-$(function () {
+$(document).ready(function () {
+
+  $(function () {
     $('[data-toggle="popover"]').popover()
-  })
+  });
+
+  $(".btn-outline-danger").on('click', function () {
+    let deleteModal = new bootstrap.Modal(document.getElementById('deleteModal'));
+    deleteModal.show();
+  });
+
+  setTimeout(function () {
+    let messages = document.getElementById("msg");
+    let alert = new bootstrap.Alert(messages);
+    alert.close();
+  }, 3000);
+
+});
